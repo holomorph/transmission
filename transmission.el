81412c4b11b8914a57874a8d213da3fec2eb7a42
Mark Oteiza <mvoteiza@udel.edu>
2015-01-03 18:41:07 -0500

    wrap prev and next

--- transmission.el
+++ transmission.el
@@ -207,6 +207,18 @@ rate."
 
 ;; Interactive
 
+(defun transmission-next ()
+  (interactive)
+  (pcase (get-text-property (point) 'transmission)
+    ('torrent (transmission-next-torrent))
+    (_ (forward-line))))
+
+(defun transmission-previous ()
+  (interactive)
+  (pcase (get-text-property (point) 'transmission)
+    ('torrent (transmission-previous-torrent))
+    (_ (forward-line -1))))
+
 (defun transmission-next-torrent ()
   "Skip to the next torrent."
   (interactive)
@@ -347,7 +359,8 @@ rate."
           (insert entry)
           (transmission-add-properties start end 'id id)))
       (setq index (1+ index)))
-    (goto-char old-point)))
+    (goto-char old-point))
+  (transmission-add-properties (point-min) (point-max) 'transmission 'torrent))
 
 (defun transmission-refresh ()
   (interactive)
@@ -359,9 +372,11 @@ rate."
 (defvar transmission-mode-map
   (let ((map (make-sparse-keymap)))
     (suppress-keymap map)
-    (define-key map "\t" 'transmission-next-torrent)
-    (define-key map [backtab] 'transmission-previous-torrent)
+    (define-key map "\t" 'transmission-next)
+    (define-key map [backtab] 'transmission-previous)
     (define-key map "\e\t" 'transmission-previous-torrent)
+    (define-key map "p" 'transmission-previous)
+    (define-key map "n" 'transmission-next)
     (define-key map "?" 'describe-mode)
     (define-key map "a" 'transmission-add)
     (define-key map "d" 'transmission-set-download)
