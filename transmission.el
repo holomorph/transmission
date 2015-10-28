d2644cb86e751d16b5f356d5b53c328d8e292d75
Mark Oteiza <mvoteiza@udel.edu>
2015-10-28 15:41:16 -0400

    prepare 0.6

--- transmission.el
+++ transmission.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2014-2015  Mark Oteiza <mvoteiza@udel.edu>
 
 ;; Author: Mark Oteiza <mvoteiza@udel.edu>
-;; Version: 0.5
+;; Version: 0.6
 ;; Package-Requires: ((emacs "24.4") (let-alist "1.0.3"))
 ;; Keywords: comm, tools
 
