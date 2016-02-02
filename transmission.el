7b9564dd209f04e32b42186a4b3eb1174264ec21
Mark Oteiza <mvoteiza@udel.edu>
2016-02-02 17:01:33 -0500

    update commentary
    * the initial json rpc has been adapted quite a bit
    * mention menu items, peer info, and pieces customization
    * mention rtorrent

--- transmission.el
+++ transmission.el
@@ -24,32 +24,35 @@
 
 ;; Interface to a Transmission session.
 
-;; Based on the JSON RPC library written by Christopher Wellons,
-;; available online here: <https://github.com/skeeto/elisp-json-rpc>
+;; Originally based on the JSON RPC library written by Christopher
+;; Wellons, available online at
+;; <https://github.com/skeeto/elisp-json-rpc>
 
 ;; Entry points are the `transmission' and `transmission-add'
 ;; commands.  A variety of commands are available for manipulating
 ;; torrents and their contents, some of which can be applied over
-;; multiple items by selecting them within a region.
+;; multiple items by selecting them within a region.  The menus for
+;; each context provide good exposure.
 
 ;; "M-x transmission RET" pops up a torrent list.  One can add,
 ;; start/stop, verify, remove torrents, set speed limits, ratio
 ;; limits, bandwidth priorities, trackers, etc.  Also, one can
-;; navigate to either a file list or torrent info context.  In the
-;; file list, individual files can be toggled for download, and their
-;; priorities set.
+;; navigate to the corresponding file list, torrent info, or peer info
+;; contexts.  In the file list, individual files can be toggled for
+;; download, and their priorities set.
 
-;; Customize-able are the session address components, RPC credentials,
-;; the display of dates, file sizes and transfer rates, and the
-;; refreshing of the torrent list.  See the `transmission'
-;; customization group.
+;; Customize-able are: the session address components, RPC
+;; credentials, the display format of dates, file sizes and transfer
+;; rates, pieces display, automatic refreshing of the torrent
+;; list, etc.  See the `transmission' customization group.
 
-;; The design draws from a number of sources, including the
-;; "transmission-remote" command line utility and the
-;; "transmission-remote-cli" ncurses interface.  These can be found
-;; respectively at the following:
+;; The design draws from a number of sources, including the command
+;; line utility transmission-remote(1), the ncurses interface
+;; transmission-remote-cli(1), and the rtorrent(1) client.  These can
+;; be found respectively at the following:
 ;; <https://trac.transmissionbt.com/browser/trunk/daemon/remote.c>
 ;; <https://github.com/fagga/transmission-remote-cli>
+;; <https://rakshasa.github.io/rtorrent/>
 
 ;;; Code:
 
