2dc52e95fc09ec6e545459d1394b53d9afa1d71f
Mark Oteiza <mvoteiza@udel.edu>
2016-05-11 12:48:34 -0400

    transmission-context: use assertion

--- transmission.el
+++ transmission.el
@@ -1380,25 +1380,25 @@ Also run the timer for timer object `transmission-timer'."
 
 (defmacro transmission-context (mode)
   "Switch to a context buffer of mode MODE."
-  (let ((name (format "*%s*" (replace-regexp-in-string "-mode\\'" ""
-                                                       (symbol-name mode)))))
+  (cl-assert (string-suffix-p "-mode" (symbol-name mode)))
+  (let ((name (make-symbol "name")))
     `(let ((id (or transmission-torrent-id
                    (cdr (assq 'id (tabulated-list-get-id)))))
-           (buffer (or (get-buffer ,name)
-                       (generate-new-buffer ,name))))
-       (if (not id)
-           (user-error "No torrent selected")
-         (with-current-buffer buffer
-           (let ((old-id (or transmission-torrent-id
-                             (cdr (assq 'id (tabulated-list-get-id))))))
-             (unless (eq major-mode ',mode)
-               (funcall #',mode))
-             (if (and old-id (eq old-id id))
-                 (revert-buffer)
-               (setq transmission-torrent-id id)
-               (transmission-draw)
-               (goto-char (point-min)))))
-         (pop-to-buffer-same-window buffer)))))
+           (,name ,(string-remove-suffix "-mode" (symbol-name mode))))
+       (if (not id) (user-error "No torrent selected")
+         (let ((buffer (or (get-buffer ,name)
+                           (generate-new-buffer ,name))))
+           (with-current-buffer buffer
+             (let ((old-id (or transmission-torrent-id
+                               (cdr (assq 'id (tabulated-list-get-id))))))
+               (unless (eq major-mode ',mode)
+                 (funcall #',mode))
+               (if (and old-id (eq old-id id))
+                   (revert-buffer)
+                 (setq transmission-torrent-id id)
+                 (transmission-draw)
+                 (goto-char (point-min)))))
+           (pop-to-buffer-same-window buffer))))))
 
 
 ;; Major mode definitions
