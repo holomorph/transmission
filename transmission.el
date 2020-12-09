deb7090e1e3faf50b691c5b73148be690575c76b
Mark Oteiza <mvoteiza@udel.edu>
2020-12-08 23:58:15 -0500

    tag 0.12.2

--- transmission.el
+++ transmission.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2014-2020  Mark Oteiza <mvoteiza@udel.edu>
 
 ;; Author: Mark Oteiza <mvoteiza@udel.edu>
-;; Version: 0.12.1
+;; Version: 0.12.2
 ;; Package-Requires: ((emacs "24.4") (let-alist "1.0.5"))
 ;; Keywords: comm, tools
 
